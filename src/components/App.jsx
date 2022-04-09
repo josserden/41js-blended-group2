493838391eb2bc2ce36c2775da7e55ee3dcb885d
Den <52295780+josserden@users.noreply.github.com>
2022-04-09 12:48:39 +0300

    create some fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
+import { useFetch } from 'use-http';
+import { Container } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
+
 export const App = () => {
+  const {
+    isLoading,
+    data = [],
+    error,
+  } = useFetch(
+    'https://624dade777abd9e37c7c8930.mockapi.io/comments?page=1&limit=10',
+    [],
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Container maxW="sm">
+      <Text as="h1" fontSize="4xl">
+        Better Comments
+      </Text>
+
+      {isLoading && <h2>Loading ...</h2>}
+
+      {error && (
+        <h3>{`There is a problem fetching the post data - ${error}`}</h3>
+      )}
+
+      <ul>{console.log(data)}</ul>
+    </Container>
   );
 };
